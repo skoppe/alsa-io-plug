04d5686c3f6d6eac2eb5e1e689ddb90878091065
Sebastiaan Koppe <mail@skoppe.eu>
2018-02-18 21:32:19 +0100

    basic plugin that errors

---
+++ source/plugin.d
@@ -0,0 +1,21 @@
+import std.stdio;
+import deimos.alsa.pcm;
+import deimos.alsa.conf;
+import deimos.alsa.global;
+import core.stdc.errno;
+import std.string;
+
+
+extern (C) {
+  alias snd_lib_error_handler_t = void function(const char *file, int line, const char *f, int err, const char *fmt,...);
+  extern snd_lib_error_handler_t snd_lib_error;
+  export int _snd_pcm_test_open (snd_pcm_t **pcmp, const char *name,
+                                 snd_config_t *root, snd_config_t *conf,
+                                 snd_pcm_stream_t stream, int mode)  {
+    snd_lib_error(__FILE__.toStringz, __LINE__, __FUNCTION__.toStringz, 0, "Stuff bla stuff".toStringz);
+    return -EINVAL;
+  }
+  export char __snd_pcm_test_open_dlsym_pcm_001;
+}
+
+
